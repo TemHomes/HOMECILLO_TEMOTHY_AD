9ca91970ef042d2c979aedddaba71c2331289441
TimHomes <temothy.homecillo.20@usjr.edu.ph>
2021-09-11 00:32:08 +0800

    3-nest Assignment
    This reverts commit b6e11df6edd82c70fdf61b955ab8b349d3a6504f.

--- 3-nest/src/exercise3/exercise3.service.ts
+++ 3-nest/src/exercise3/exercise3.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class Exercise3Service {
     helloWorld(){
-        console.log("Hello Za Warudo!! Toki wo tomare!!!");
         return "Hello Za Warudo!! Toki wo tomare!!!";
     }
     loopsTriangle(height:number){
